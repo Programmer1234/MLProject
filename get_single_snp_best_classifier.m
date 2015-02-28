3ee4fee39ad854e2152ed190f40806a0b4d4f1f0
Shahaf Gonen <shahafgonen73142@gmail.com>
2015-02-28 19:02:25 +0200

    Changes main.m, added support for selecting different classifying methods for different SNPs :)

---
+++ get_single_snp_best_classifier.m
@@ -0,0 +1,36 @@
+function [best_classifier, best_cls_method, best_success_rate] = get_single_snp_best_classifier(train_samples, train_labels)
+
+K = 10;  % Fold validation constant used (k-fold-validation is performed)
+
+% Initialize
+best_success_rate = 0;
+best_classifier = 0;
+best_cls_method = 0;
+
+% Possible classifiers.
+CLASSIFIERS = {'svm-one-vs-one'; 'random'};
+
+% Run classifiers
+for i = 1 : length(CLASSIFIERS)
+    
+    % Get the current classifier
+    curr_cls_method = CLASSIFIERS{i};
+    
+    % Train the classifier
+    curr_classifier = train_classifier_for_single_snp(train_samples, train_labels, curr_cls_method);
+    
+    % Calculate it's success rate
+    curr_classifier_sucess_rate = k_fold_validation(train_samples, train_labels, K, curr_cls_method);
+    
+    fprintf('For %s, success rate is: %.2f\n', curr_cls_method, curr_classifier_sucess_rate * 100);
+    
+    if curr_classifier_sucess_rate > best_success_rate
+        best_success_rate = curr_classifier_sucess_rate;
+        best_classifier = curr_classifier;
+        best_cls_method = curr_cls_method;
+    end
+end
+
+
+end
+
--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-function [best_classifier_predicted_labels, best_success_rate] = main()
+function [predicted_labels_mat, success_rate_vec, used_classifiers_vec, avg_success_rate] = main()
 % Project's entry point
 
 % Adding folders to search path
@@ -6,29 +6,42 @@ addpath Classifiers;
 addpath Classifiers\svm;
 addpath Classifiers\Adaboost;
 
-% Classifiers selected.
-CLASSIFIERS = {'svm-one-vs-one'; 'random'};
-
 % load data - extracted_test, extraced_train, missing
 load Data\dataforproject.mat;
 
 % Parse input
 [train_data, train_labels, test_data, ~] = parse_input(extracted_train, extracted_test);
+nSNPs = size(train_labels, 1);
 
-best_success_rate = 0;
-best_classifier_predicted_labels = 0;
+% Initialize out parameters
+success_rate_vec = zeros(nSNPs, 1);
+predicted_labels_mat = zeros(nSNPs, 400);
+used_classifiers_vec = {nSNPs, 1};
 
-% Run classifiers
-for i = 1 : length(CLASSIFIERS)
-    [test_predicted_labels_mat, success_rate] = run_classifier(train_data, train_labels, test_data, CLASSIFIERS{i});
-    fprintf('Classifier %s success rate is: %.2f\n', CLASSIFIERS{i}, 100* success_rate);
+for i = 1 : nSNPs
+    fprintf('For SNP %d, selecting the best classifier\n', i);
     
-    if success_rate > best_success_rate
-        best_success_rate = success_rate;
-        best_classifier_predicted_labels = test_predicted_labels_mat;
-    end
+    % Transpoe is made as each sample is in each row
+    current_snp_train_samples = squeeze(train_data(i, : ,:))';
+    current_snp_train_labels = squeeze(train_labels(i, :))';
+    current_snp_test_samples = squeeze(test_data(i, : ,:))';
+    
+    [snp_classifier, snp_best_cls_method, snp_success_rate] = get_single_snp_best_classifier(current_snp_train_samples, current_snp_train_labels);
+    
+    snp_preditcted_labels = predict_labels_for_single_snp(current_snp_test_samples, snp_classifier, snp_best_cls_method);
+    
+    fprintf('For SNP %d, best success rate: %.2f, achieved with %s\n\n', i, 100* snp_success_rate, snp_best_cls_method);   
+    
+    % Save results
+    success_rate_vec(i) = snp_success_rate;
+    predicted_labels_mat(i, :) = snp_preditcted_labels;
+    used_classifiers_vec{i} = snp_best_cls_method;
 end
 
-fprintf('Best success rate is: %.2f\n', 100*best_success_rate);
+% Final calculations...
+used_classifiers_vec = used_classifiers_vec';
+avg_success_rate = mean(success_rate_vec);
+
+fprintf('Average success rate (across different SNPs) is %.2f\n', 100 * avg_success_rate);
 
 end
